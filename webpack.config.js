4c930f934d276e1f58c6b5b512503833015a5553
Petrov Dmitry <numb-94@mail.ru>
2018-05-07 22:22:04 +0400

    init default development tools

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const NODE_ENV = process.env.NODE_ENV || 'dev';
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: "./app/app.js", // входная точка - исходный файл
+  output: {
+    path: path.resolve(__dirname, './public'),     // путь к каталогу выходных файлов - папка public
+    publicPath: '/public/',
+    filename: "[name].js"       // название создаваемого файла
+  },
+  devtool: NODE_ENV === 'dev' ? 'source-map' : '',
+  module: {
+    rules: [   //загрузчик для jsx
+      {
+        test: /\.jsx?$/, // определяем тип файлов
+        exclude: /(node_modules)/,  // исключаем из обработки папку node_modules
+        use: {
+          loader: "babel-loader",   // определяем загрузчик
+          options: {
+            presets: ["es2015", "env", "react", "stage-3"]    // используемые плагины
+          }
+        }
+      },
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          use: "css-loader!less-loader"
+        })
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(NODE_ENV)
+    }),
+    new ExtractTextPlugin("styles.css")
+  ]
+};
+
+if (NODE_ENV === 'prod') {
+  module.exports.plugins.push(
+      new UglifyJsPlugin()
+  )
+}
